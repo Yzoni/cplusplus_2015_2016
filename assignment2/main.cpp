53edc88e7c07d69ee230e6126fdfdbcf788c313e
Yzoni <yzonix@gmail.com>
2016-03-12 23:53:10 +0100

    assignment2: add decompress

--- assignment2/main.cpp
+++ assignment2/main.cpp
@@ -9,16 +9,19 @@
 
 using namespace std;
 
-int main() {
+int decompress(string inputfile, string outputfile);
 
+int compress(string inputfile, string outputfile);
+
+int compress(string inputfile, string outputfile) {
     ifstream inStream;
     ofstream outStream;
 
-    inStream.open("/home/yorick/ClionProjects/cmethoden/assignment2/original.txt");
+    inStream.open(inputfile);
     if (inStream.fail()) {
         exit(1);
     }
-    outStream.open("/home/yorick/ClionProjects/cmethoden/assignment2/compressed.txt");
+    outStream.open(outputfile);
     if (outStream.fail()) {
         exit(1);
     }
@@ -32,14 +35,17 @@ int main() {
         if (next == previous) {
             count += 1;
         } else {
+            // If two or more the same characters are found after each other
             if (count != 1) {
-                if ((int) previous == 92 || ((int) previous >= 48 && (int) previous <= 57)) {
+                // Character is a slash or an integer
+                if (previous == '\\' || (previous >= '0' && previous <= '9')) {
                     outStream << "\\"  << previous << count;
                 } else {
                     outStream << previous << count;
                 }
             } else {
-                if ((int) previous == 92 || ((int) previous >= 48 && (int) previous <= 57)) {
+                // Character is a slash or an integer
+                if (previous == '\\' || (previous >= '0' && previous <= '9')) {
                     outStream << "\\" << previous;
                 } else {
                     outStream << previous;
@@ -55,4 +61,62 @@ int main() {
     outStream.close();
 
     return 0;
+}
+
+int decompress(string inputfile, string outputfile) {
+    ifstream inStream;
+    ofstream outStream;
+    inStream.open(inputfile);
+    if (inStream.fail()) {
+        exit(1);
+    }
+    outStream.open(outputfile);
+    if (outStream.fail()) {
+        exit(1);
+    }
+
+    int tempCount;
+    int count = -1;
+    bool escapeFlag = false;
+    char lastChar;
+    char currentChar;
+
+    inStream.get(currentChar);
+    lastChar = currentChar;
+    while (!inStream.eof()) {
+        if (currentChar >= '0' && currentChar <= '9' && !escapeFlag) {
+            escapeFlag = false;
+            tempCount = count;
+            count = currentChar - '0'; // Convert back to integer
+            if (tempCount > 0) {
+                count = (tempCount * 10) + count;
+            }
+        } else if (currentChar == '\\' and !escapeFlag) {
+            escapeFlag = true;
+        } else {
+            escapeFlag = false;
+            if (count > 0) {
+                count--;
+            }
+            for (int i = -1; i < count; i++) {
+                outStream << lastChar;
+            }
+            count = 0;
+            lastChar = currentChar;
+        }
+        inStream.get(currentChar);
+    }
+    outStream << currentChar;
+
+    inStream.close();
+    outStream.close();
+
+    return 0;
+}
+
+int main() {
+    compress("/home/yorick/ClionProjects/cmethoden/assignment2/original.txt",
+             "/home/yorick/ClionProjects/cmethoden/assignment2/compressed.txt");
+    decompress("/home/yorick/ClionProjects/cmethoden/assignment2/compressed.txt",
+               "/home/yorick/ClionProjects/cmethoden/assignment2/decompressed.txt");
 }
